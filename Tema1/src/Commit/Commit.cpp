e9f7fdeb7b2e1878a6e8d01c3e36a4bb780742ca
cristinagusita <cristinagusita23@gmail.com>
2023-10-27 17:03:13 +0300

    Dinamically allocated char arrays

--- Tema1/src/Commit/Commit.cpp
+++ Tema1/src/Commit/Commit.cpp
@@ -1,17 +1,20 @@
 #include "Commit.h"
 #include<iostream>
+#include<cstring>
 
-Commit::Commit(int id, const std::string message, int data) {
+Commit::Commit(int id, const char* message, int data) {
     std::cout<<"I am the commit constructor!"<<"\n";
     this->id = id;
-    this->message = message;
+    this->message = new char[strlen(message)+1];
+    strcpy(this->message, message);
     this->data = data;
 }
 
 Commit::Commit(const Commit& other) {
-    std::cout<<"I am the COPY constructor!"<<"\n";
+        std::cout<<"I am the COPY constructor!"<<"\n";
         this->id = other.id;
-        this->message = other.message;
+        this->message = new char[strlen(other.message)+1];
+        strcpy(this->message, other.message);
         this->data = other.data;
     }
 
@@ -21,19 +24,20 @@ Commit::Commit(Commit&& other) noexcept {
         this->message = other.message;
         this->data = other.data;   
         other.id = 0;
-        other.message = "";
+        other.message = nullptr;
         other.data = 0;
 }
 
 Commit::~Commit() {
     std::cout<<"I am the commit destructor!"<<"\n";
+    delete[] message;
 }
 
 int Commit::getId() const {
     return id;
 }
 
-std::string Commit::getMessage() const {
+char* Commit::getMessage() const {
     return message;
 }
 
--- Tema1/src/Commit/Commit.h
+++ Tema1/src/Commit/Commit.h
@@ -4,16 +4,16 @@
 class Commit {
 private:
     int id;
-    std::string message;
+    char* message;
     int data; 
 
 public:
-    Commit(int id, const std::string message, int data);
+    Commit(int id, const char* message, int data);
     Commit(const Commit& other); 
     Commit(Commit&& other) noexcept; 
     ~Commit();
     int getId() const;
-    std::string getMessage() const;
+    char* getMessage() const;
     int getData() const;
     void exampleModifyMessage();
 
