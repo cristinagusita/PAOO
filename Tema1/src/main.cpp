08cf086fbb17ad07767f66145c2cb9541fdd3f56
cristinagusita <cristinagusita23@gmail.com>
2023-11-29 16:14:04 +0200

    Added examples for thread synchronization and smart pointers

--- Tema1/src/main.cpp
+++ Tema1/src/main.cpp
@@ -14,19 +14,40 @@ using MyGit::MergeCommit;
 int main() {
     Repository rep;
 
-    Commit* featureCommit = new FeatureCommit("Added a new feature", "The feature X now manages to blablabla");
+    // Create threads for each addCommit
+    std::thread thread1([&rep]() {
+        rep.addCommit(std::make_shared<FeatureCommit>("Add new feature", "Add new feature details"));
+    });
 
-    Commit* bugCommit = new BugFixCommit("Solved a bug", "This bug fix blablabla");
+    std::thread thread2([&rep]() {
+        rep.addCommit(std::make_shared<BugFixCommit>("Fix bug", "Fix bug details"));
+    });
 
-    Commit* mergeCommit = new MergeCommit("Merged feature X with master", "This merge commit blablabla");
+    std::thread thread3([&rep]() {
+        rep.addCommit(std::make_shared<MergeCommit>("Merge branch", "Merge branch details"));
+    });
 
-    rep.addCommit(featureCommit);
-    rep.addCommit(bugCommit);
-    rep.addCommit(mergeCommit);
+    // Wait for the threads to finish
+    thread1.join();
+    thread2.join();
+    thread3.join();
 
-    for (auto commit : rep.getCommits()) {
-        commit->display();
+
+    auto featureCommitPtr = rep.getCommitById(1);
+    if (featureCommitPtr) {
+        auto featureCommitMessage = featureCommitPtr->getMessage().lock(); // Lock weak_ptr to shared_ptr
+        if (featureCommitMessage) {
+            std::cout << featureCommitMessage.get() << "\n"; // Access the message
+        } else {
+            std::cout << "No message available." << "\n";
+        }
+    } else {
+        std::cout << "Commit with ID 0 not found." << "\n";
     }
+
+
+
+    rep.processCommitsInParallel();
     
 
     return 0;
